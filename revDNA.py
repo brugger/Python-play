678fceea8229dde351f990c47a3794e4b58316f8
gleadalln <gleadalln@cam.ac.uk>
2014-11-11 12:18:15 +0000

    finished function

--- revDNA.py
+++ revDNA.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python
 
 
+
+def seq_reverse(sequence):
+
+	rev_sequence = []
+
+	dna_dict = {'A' : 'T', 'T' : 'A', 'C' : 'G', 'G' : 'C'}
+
+	for base in sequence:
+		if base in dna_dict.keys():
+			rev_sequence.append(dna_dict[base])
+		else:
+			rev_sequence.append(base)
+
+	"".join(rev_sequence)
+
+	trev_sequence = rev_sequence[::-1]		
+
+	
+	return "".join(trev_sequence)
+
+
 print "hello world"
+print seq_reverse("AATTNCCGG")
 
-
-def revDNA(sequence):
-	pass
